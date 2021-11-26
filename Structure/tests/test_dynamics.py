5071aa2e3d37a772aaa20c9e200e817436b9e7b3
pedrogil <pedrogil@pedro-HP>
2021-11-26 14:59:01 +0100

    Correct test_dynamics program

--- Structure/tests/test_dynamics.py
+++ Structure/tests/test_dynamics.py
@@ -17,26 +17,32 @@ image = numpy.full((500, 800, 3), 0xFF, numpy.uint8)
 landing = 2000.0
 stair_list = [
     {'N': 2, 'd': 250.0, 'w': 100.0, 'h': +40.0}
-    ]
+]
 size = {
     'a': 40,
     'b': 80,
     'c': 30,
     'd': 50,
-    'g': 50}
+    'g': 50,
+    'm': 1}
 wheels = {
     'r1': 15,
     'r2': 15,
     'r3': 15,
     'r4': 15}
-stair_test = stairs.Stair(stair_list, landing)                
+stair_test = stairs.Stair(stair_list, landing)
 base_test = base.Base(size, wheels, stair_test)
 
 speed_data = {
-    'wheel': 5.0,
-    'actuator': 1.0,
-    'struct_up': 0.5,
-    'struct_down': 1.0}
+    'wheel': 2.0,
+    'actuator_up': 2.0,
+    'actuator_dw': 3.0,
+    'elevate_up': 0.5,
+    'elevate_dw': 1.0,
+    'incline_up': 0.4,
+    'incline_dw': 0.8,
+    'sample_time': 1.0}
+
 sm = Simulator(speed_data)
 
 image[:] = 0xFF
@@ -45,7 +51,7 @@ base_test.draw((0, 400), image, 8, 3)
 cv2.imshow("image", image)
 cv2.waitKey()
 
-command = {'distance': 200,'elevate': 10,'incline': 20}
+command = {'distance': 200, 'elevate': 10, 'incline': 20}
 y = 0
 for res in sm.simulate_instruction(base_test, command):
     image[:] = 0xFF
@@ -54,6 +60,6 @@ for res in sm.simulate_instruction(base_test, command):
     cv2.imshow("image", image)
     cv2.waitKey(5)
     print(y)
-    y+=1
+    y += 1
     if y == 410:
         pass
